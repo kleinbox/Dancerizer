1f2a8d4180a714bc729588073c68e77da6bd18a4
Sammy <SammyKoch@pm.me>
2024-07-12 15:58:00 +0200

    bump v1.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import java.time.LocalDate
 
 object Constants {
 	// Mod Properties
-	const val VERSION = "1.2.0"
+	const val VERSION = "1.2.1"
 	const val GROUP = "dev.kleinbox"
 	const val MODID = "dancerizer"
 
