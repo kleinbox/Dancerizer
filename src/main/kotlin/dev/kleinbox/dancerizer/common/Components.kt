665a7fe4933ac3502cdf5c4dd4b9bed6fc0d3c43
Sammy <SammyKoch@pm.me>
2024-07-12 16:10:56 +0200

    fix: dance component

--- src/main/kotlin/dev/kleinbox/dancerizer/common/Components.kt
+++ src/main/kotlin/dev/kleinbox/dancerizer/common/Components.kt
@@ -1,5 +1,6 @@
 package dev.kleinbox.dancerizer.common
 
+import com.mojang.datafixers.util.Pair
 import com.mojang.serialization.Codec
 import net.minecraft.core.component.DataComponentType
 import net.minecraft.core.registries.BuiltInRegistries
@@ -8,10 +9,14 @@ import net.minecraft.network.codec.ByteBufCodecs
 @Suppress("UNCHECKED_CAST")
 object Components : RegisteringContainer<DataComponentType<out Any>>(BuiltInRegistries.DATA_COMPONENT_TYPE) {
 
-    val DANCE = register("dance", DataComponentType.builder<com.mojang.datafixers.util.Pair<String, Int>>()
-        .persistent(Codec.pair(Codec.STRING, Codec.INT))
-        .networkSynchronized(ByteBufCodecs.fromCodecWithRegistries(Codec.pair(Codec.STRING, Codec.INT)))
-        .build()) as DataComponentType<com.mojang.datafixers.util.Pair<String, Int>>
+    val DANCE = run {
+        val codec = Codec.mapPair(Codec.STRING.fieldOf("dance"), Codec.INT.fieldOf("duration")).codec()
+        register("dance", DataComponentType.builder<Pair<String, Int>>()
+                .persistent(codec)
+                .networkSynchronized(ByteBufCodecs.fromCodecWithRegistries(codec))
+                .build()
+        ) as DataComponentType<Pair<String, Int>>
+    }
 
     val TAUNT = register("taunt", DataComponentType.builder<String>()
         .persistent(Codec.STRING)
