6df5903599b7c6db7990ab682e31ad355e004b21
Sammy <SammyKoch@pm.me>
2024-07-12 16:46:14 +0200

    fix: particles for everyone

--- src/main/kotlin/dev/kleinbox/dancerizer/common/compat/ConfettiEmitterImplementation.kt
+++ src/main/kotlin/dev/kleinbox/dancerizer/common/compat/ConfettiEmitterImplementation.kt
@@ -6,6 +6,8 @@ import dev.doublekekse.confetti.packet.ExtendedParticlePacket
 import dev.kleinbox.dancerizer.Dancerizer.logger
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking
 import net.minecraft.server.level.ServerPlayer
+import net.minecraft.world.entity.player.Player
+import net.minecraft.world.phys.AABB
 import net.minecraft.world.phys.Vec3
 
 
@@ -16,28 +18,40 @@ object ConfettiEmitterImplementation : ConfettiEmitter {
     }
 
     override fun particlePop(player: ServerPlayer) {
-        ServerPlayNetworking.send(
-            player,
-            ExtendedParticlePacket(
-                Vec3Dist(player.position().add(0.0, 0.5, 0.0), 0.05),
-                Vec3Dist(Vec3(0.0, 0.5, 0.0), 0.2),
-                50,
-                false,
-                Confetti.CONFETTI
+        val range = 50.0
+        val bounds = player.boundingBox.inflate(range, range, range)
+        val players = player.level().getEntitiesOfClass(player.javaClass, bounds)
+
+        players.forEach {
+            ServerPlayNetworking.send(
+                it,
+                ExtendedParticlePacket(
+                    Vec3Dist(player.position().add(0.0, 0.5, 0.0), 0.05),
+                    Vec3Dist(Vec3(0.0, 0.5, 0.0), 0.2),
+                    50,
+                    false,
+                    Confetti.CONFETTI
+                )
             )
-        )
+        }
     }
 
     override fun particleRain(player: ServerPlayer) {
-        ServerPlayNetworking.send(
-            player,
-            ExtendedParticlePacket(
-                Vec3Dist(player.position().add(0.0, 1.0, 0.0), 0.05),
-                Vec3Dist(Vec3(0.0, 0.8, 0.0), 0.1),
-                1,
-                false,
-                Confetti.CONFETTI
+        val range = 50.0
+        val bounds = player.boundingBox.inflate(range, range, range)
+        val players = player.level().getEntitiesOfClass(player.javaClass, bounds)
+
+        players.forEach {
+            ServerPlayNetworking.send(
+                it,
+                ExtendedParticlePacket(
+                    Vec3Dist(player.position().add(0.0, 1.0, 0.0), 0.05),
+                    Vec3Dist(Vec3(0.0, 0.6, 0.0), 0.8),
+                    1,
+                    false,
+                    Confetti.CONFETTI
+                )
             )
-        )
+        }
     }
 }
